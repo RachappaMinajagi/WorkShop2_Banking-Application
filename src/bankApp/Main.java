945d884dddbf266f42c13e59fb63eb169a25c593
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-08 12:00:46 +0530

    Banking Application Program

---
+++ src/bankApp/Main.java
@@ -0,0 +1,34 @@
+package bankApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Creating Object for Saver1 and Saver 2
+		 */
+
+		SavingAccount saver1 = new SavingAccount(2000);
+		SavingAccount saver2 = new SavingAccount(3000);
+
+		/*
+		 * / SavingAccount.annuallInterestRate = 4 %;
+		 */
+
+		SavingAccount.modifyInterestRate(4);
+        saver1.calculateMonthlyInterest();
+		saver2.calculateMonthlyInterest();
+		saver1.displaySavings();
+		saver2.displaySavings();
+
+		/*
+		 * / SavingAccount.annuallInterestRate = 5 %;
+		 */
+
+		SavingAccount.modifyInterestRate(5);
+		saver1.calculateMonthlyInterest();
+		saver2.calculateMonthlyInterest();
+		saver1.displaySavings();
+		saver2.displaySavings();
+
+	}
+}
--- src/bankApp/SavingAccount.java
+++ src/bankApp/SavingAccount.java
@@ -1,9 +1,48 @@
 package bankApp;
 
+/*
+ * using static variable AnnualInterestrate to store the annual interest rate
+ * for all the account holders and each object of the class contains private
+ * instance variable savingBalance representing the amount the saver currently
+ * has on deposit
+ */
+
 public class SavingAccount {
+
+	private static double annuallInterestRate;
+	private double savingsBalance;
+	private double monthlyInterest;
+/*
+ * creating constructor of SavingAccount class having the same name as the class without return type
+ */
+	public SavingAccount(double savingsBalance) {
+		this.savingsBalance = savingsBalance;
+
+	}
+/*
+ * Create Method calculate Monthly Interest
+ */
+	public void calculateMonthlyInterest() {
+		this.monthlyInterest = (savingsBalance * annuallInterestRate/100) / 12;
+		System.out.println("Annual InterestRate: " + this.annuallInterestRate);
+	}
 	
-	/**
-	 * Static variables - annual interest rate offered by bank.
+	/*
+	 * creating a static method modifyInterestRate that sets the annualInterestRate
+	 * to a new value
 	 */
-	public static double annualInterestRate = 4;
+
+	public static void modifyInterestRate(double interestRate) {
+		annuallInterestRate = interestRate;
+	}
+
+	private void calculateSavings() {
+		savingsBalance += this.monthlyInterest;
+	}
+
+	public void displaySavings() {
+		calculateSavings();
+		System.out.println("The total balance is :" + savingsBalance);
+	}
+
 }
